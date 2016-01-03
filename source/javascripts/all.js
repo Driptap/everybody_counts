7a64d9d92b94ab82cc675344e6e0d8b789402de4
driptap <sg.sinan@gmail.com>
2016-01-03 17:22:41 +0100

    Accounter initial

---
+++ source/javascripts/all.js
@@ -0,0 +1,297 @@
+//= require_tree .
+
+
+var config = {
+  "descriptors" : [
+    {
+      "descriptor": ["MAJOR"],
+      "toAccount": "Bank Account",
+      "fromAccount": "Sales Account",
+      "reference": "",
+      "customer": "Ammanda Major"
+    },
+    {
+      "descriptor": ["COBBETT HILL"],
+      "toAccount": "Bank Account",
+      "fromAccount": "Sales Account",
+      "reference": "",
+      "customer": "Cobbett Hill Earth Station"
+    },
+    {
+      "descriptor": ["B AND Q"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "B&Q"
+    },
+    {
+      "descriptor": ["VIEWSAT"],
+      "toAccount": "Bank Account",
+      "fromAccount": "Sales Account",
+      "reference": "",
+      "customer": "Viewsat"
+    },
+    {
+      "descriptor": ["COVERS"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Covers"
+    },
+    {
+      "descriptor": ["PAYPAL"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Ebay"
+    },
+    {
+      "descriptor": ["ELECTRICL CENTER"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Electric Center"
+    },
+    {
+      "descriptor": ["ENTERPRISE SEC"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Enterprice SEC Security"
+    },
+    {
+      "descriptor": ["EUROSAT"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Euro Sat"
+    },
+    {
+      "descriptor": ["EYRE AND ELLISON"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Eyre and Elliston"
+    },
+    {
+      "descriptor": ["FOCUS"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "FOCUS"
+    },
+    {
+      "descriptor": ["HOMEBASE"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Homebase"
+    },
+    {
+      "descriptor": ["JBM", "EDMUNDSON"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "JBM Edmunsons"
+    },
+    {
+      "descriptor": ["KEITH GRAHAM"],
+      "toAccount": "Legal and Profressional Costs Account",
+      "fromAccount": "Bank Account",
+      "supplier": "Keith Graham Accountants"
+    },
+    {
+      "descriptor": ["NETWORK"],
+      "toAccount": "Motor Expense Account",
+      "fromAccount": "Bank Account",
+      "supplier": "Network Hire",
+      "reference": "Van lease"
+    },
+    {
+      "descriptor": ["AA MEMBERSHIP"],
+      "toAccount": "Motor Expense Account",
+      "fromAccount": "Bank Account",
+    },
+    {
+      "descriptor": ["QVS"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "QVS Electrical"
+    },
+    {
+      "descriptor": ["SCREWFIX"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Screwfix"
+    },
+    {
+      "descriptor": ["W F ELECTRICAL", "WF"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "W F Electrical"
+    },
+    {
+      "descriptor": ["WICKES"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Wickes"
+    },
+    {
+      "descriptor": ["WILLIAMS"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Williams Trade Supplies"
+    },
+    {
+      "descriptor": ["WILSON"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Wilson Electrical"
+    },
+    {
+      "descriptor": ["CITY PLUMBING SUPP"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "City Plumbing Supplies"
+    },
+    {
+      "descriptor": ["CITY ELECTRICAL"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "City Electrical Factors"
+    },
+    {
+      "descriptor": ["EMC SHIELDING SOLU"],
+      "toAccount": "Bank Account",
+      "fromAccount": "Sales Account",
+      "customer": "EMC Shielding"
+    },
+    {
+      "descriptor": ["JEWSON"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Jewson"
+    },
+    {
+      "descriptor": ["VAT"],
+      "toAccount": "VAT Control Account",
+      "fromAccount": "Bank Account"
+    },
+    {
+      "descriptor": ["TLC"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "TLC"
+    },
+    {
+      "descriptor": ["LIDL", "APPLE", "GOOGLE", "WAITROSE", "THOMAS COOK", "SELFSERVE", "VIRGIN PURE", "COSTCO", "BEST MANGAL", "GRADUATION SERVICE", "DVLAPERSONALISEDRE", "LAHORI", "TESCO STORES", "TEX", "SAINSBURYS S/MKTS", "B289BDA", "LOD628W", "ALDI", "AHMET GUCLU"],
+      "toAccount": "Wages",
+      "fromAccount": "Bank Account"
+    },
+    {
+      "descriptor": ["MAPLIN"],
+      "toAccount": "Equipment",
+      "fromAccount": "Bank Account",
+      "supplier": "Maplin"
+    },
+    {
+      "descriptor": ["NICEIC", "CERTSURE"],
+      "toAccount": "Legal and Profressional Costs Account",
+      "fromAccount": "Bank Account",
+      "reference": "NicEic Costs"
+    },
+    {
+      "descriptor": ["VIRGIN MOBILE", "LYCA"],
+      "toAccount": "Telephone",
+      "fromAccount": "Bank Account"
+    },
+    {
+      "descriptor": ["BP", "ESSO", "SHELL", "SAINSBURYS PETROL", "TCS", "KINGSMEAD", "SERV", "COOP", "CO-OP"],
+      "toAccount": "Travel and Subsistence Account",
+      "fromAccount": "Bank Account",
+      "reference": "Diesel"
+    },
+    {
+      "descriptor": ["CHURCHILL", "SWINTON"],
+      "toAccount": "Motor Expense Account",
+      "fromAccount": "Bank Account",
+      "reference": "Van Insurance"
+    },
+    {
+      "descriptor": ["HMRC - NI"],
+      "toAccount": "National Insurance Contribution",
+      "fromAccount": "Bank Account",
+      "reference": "National Insurance"
+    },
+    {
+      "descriptor": ["HALIFAX", "BARCLAYCARD"],
+      "toAccount": "Credit Card",
+      "fromAccount": "Bank Account"
+    },
+    {
+      "descriptor": ["HP INSTANT INK"],
+      "toAccount": "Stationery",
+      "fromAccount": "Bank Account",
+      "reference": "Printing Supplies"
+    }
+  ]
+};
+
+var columns = {
+  'Date' : 0,
+  'Type' : 1,
+  'Description' : 2,
+  'Value' : 3,
+  'Balance' : 4,
+  'AccountName' : 5,
+  'AccountNumber' : 6
+}
+
+var fileInput = $('input#statement');
+var totalInputRecords = 0;
+var outputAccounts = [];
+// Listens for selected csv
+fileInput.on('change', function(){
+  // Calls parse on file
+  Papa.parse(fileInput[0].files[0], {
+    // On parse complete
+    complete: function(results) {
+      var accounts = results.data;
+      totalInputRecords = accounts.length;
+      // Loops through each account
+      for(var x = 0; x < accounts.length; x++){
+        // Sets exiting accoutn
+        var account = accounts[x];
+        // Creates a new account object
+        var newAccount = {
+          'date': account[columns.Date],
+          'type': account[columns.Type],
+          'value': account[columns.Value],
+          'balance': account[columns.Balance],
+          'description': account[columns.Description],
+          'reference': '',
+          'fromAccount': 'Bank Account',
+          'toAccount': 'Bank Account',
+          'supplier': '',
+          'customer': ''          
+        };
+        // Loops through account descriptors
+        var describedRecord = false;
+        var accountDescriptors = config.descriptors;
+        for(var y=0; y<accountDescriptors.length; y++) {
+          var descriptor = accountDescriptors[y];
+          // Checks regex against defined descriptors
+          if (new RegExp(descriptor.descriptor.join("|")).test(newAccount.description) && !describedRecord){ 
+            // Populates new descriptor data into new account object
+            newAccount.fromAccount = descriptor.fromAccount;
+            newAccount.toAccount = descriptor.toAccount;
+            newAccount.supplier = descriptor.supplier;
+            newAccount.customer = descriptor.customer;
+            newAccount.reference = descriptor.reference;
+            outputAccounts.push(newAccount);
+            describedRecord = true;
+          }
+        }
+        if(!describedRecord) {
+          newAccount.reference = 'NEEDS CHECK';
+          outputAccounts.push(newAccount);
+        }
+      }      
+      // Convert back to csv and export
+      console.log(outputAccounts);
+      if(totalInputRecords - outputAccounts.length > 0) {console.log(totalInputRecords - outputAccounts.length + " unprocessed records..")}
+      var newCsv = Papa.unparse(outputAccounts); 
+      download(newCsv, "accounts.csv", "text/csv");
+    }
+  });
+});
